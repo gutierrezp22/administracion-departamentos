d71405e8a97a77bf87ae51df1c45a2ca315067b6
gutierrezp22 <pabloexegutierrez@gmail.com>
2025-05-16 20:33:16 -0300

    fix login

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -169,13 +169,13 @@ export default function LoginPage() {
               </label>
             </div>
 
-            <div className="text-sm">
+            {/* <div className="text-sm">
               <a
                 href="#"
                 className="font-medium text-blue-600 hover:text-blue-500 transition-colors duration-200">
                 ¿Olvidó su contraseña?
               </a>
-            </div>
+            </div> */}
           </div>
 
           <div>
